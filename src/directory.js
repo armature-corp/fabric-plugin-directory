adc1521ff98893bd7ee4c0d78fe1e0be384558d6
Hunter Fitzwater <hcfitzwater@gmail.com>
2016-08-14 22:20:27 -0400

    simplifies directory

--- src/directory.js
+++ src/directory.js
@@ -2,17 +2,6 @@ export class Directory {
   
   heading = 'Directory';
 
-  plugins = [
-    {
-      name: 'Skeleton Plugin',
-      route: 'hello-world'
-    },
-    {
-   	  name: 'Pager',
-   	  route: 'pager'	
-    }
-  ];
-
   constructor() {
 
   }
